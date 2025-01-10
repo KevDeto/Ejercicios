48c144d562899f76237b274c0d1a40ea6449fbe7
KevDeto <contactohuayta@gmail.com>
2025-01-10 17:55:42 -0300

    realizadas las funcionalidades de la cuenta

--- Programming_Skills_Level0/src/ejercicio1/Cuenta.java
+++ Programming_Skills_Level0/src/ejercicio1/Cuenta.java
@@ -1,10 +1,39 @@
 package ejercicio1;
 
-import java.math.BigInteger;
+import java.math.BigDecimal;
 
 public class Cuenta {
-	private BigInteger saldo;
+	private BigDecimal saldo;
 	
 	public Cuenta() {
+		this.saldo = new BigDecimal(2000.0);
+	}
+	
+	public void depositarSaldo(BigDecimal deposito){
+		this.saldo = this.saldo.add(deposito);
+	}
+	
+	public void retirarSaldo(BigDecimal retiro) {
+	    if (saldo.compareTo(retiro) >= 0) {
+	        this.saldo = this.saldo.subtract(retiro);
+	        System.out.println("Retiro exitoso. Nuevo saldo: $" + this.saldo.toString());
+	    } else {
+	        System.out.println("Saldo insuficiente. Saldo actual: $" + this.saldo.toString());
+	    }
+	}
+	
+	public void verSaldo() {
+		System.out.println("$"+ this.saldo.toString());
+	}
+	
+	public void transferirSaldo(BigDecimal monto, Cuenta destino) {
+	    if (saldo.compareTo(monto) >= 0) {
+	        this.saldo = this.saldo.subtract(monto);
+	        destino.depositarSaldo(monto);
+	        
+	        System.out.println("Transferencia realizada. Nuevo saldo: $" + saldo.toString());
+	    } else {
+	        System.out.println("Saldo insuficiente.");
+	    }
 	}
 }
