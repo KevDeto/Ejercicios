8cbe55a5be3e4a004e49c04f8b3e46458ba4fa6d
KevDeto <contactohuayta@gmail.com>
2025-01-10 14:38:45 -0300

    creadas las operaciones que podra realizar el usuario

---
+++ Programming_Skills_Level0/src/ejercicio1/Operacion.java
@@ -0,0 +1,19 @@
+package ejercicio1;
+
+public class Operacion {
+
+	public Operacion(){
+	}
+	
+	public void depositar(){
+	}
+	
+	public void retirar() {
+	}
+	
+	public void ver() {
+	}
+	
+	public void transferir() {
+	}
+}
