a95201926496c49ac7d0157bfff6425c4fb8d7b1
KevDeto <contactohuayta@gmail.com>
2025-01-12 01:08:00 -0300

    se crearon funciones

--- Programming_Skills_Level0/src/ejercicio3/Campus.java
+++ Programming_Skills_Level0/src/ejercicio3/Campus.java
@@ -1,5 +1,29 @@
 package ejercicio3;
 
-public class Campus {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Campus {
+	private String nombre;
+	private Map<String, Integer> plazas;
+
+	public Campus(String nombre) {
+		this.nombre = nombre;
+		this.plazas = new HashMap<String, Integer>();
+	}
+
+	public void crearProgramas(String[] programas, Integer plaza) {
+		for (String programa : programas) {
+			plazas.put(programa, plaza);
+		}
+	}
+
+	public Integer getPlazasDisponibles(String programa) {
+		return plazas.getOrDefault(programa, 0);
+	}
+
+	public void ocuparPlaza(String programa) {
+		Integer plaza = plazas.get(programa);
+		plazas.replace(programa, plaza-1);
+	}
 }
