7ac6042bf49d3ee5e5fa1d1ff8fb4fd81b32c9ae
KevDeto <contactohuayta@gmail.com>
2025-01-12 01:11:15 -0300

    se agrego la logica que gestiona la matriculacion del usuario

--- Programming_Skills_Level0/src/ejercicio3/MatriculaService.java
+++ Programming_Skills_Level0/src/ejercicio3/MatriculaService.java
@@ -1,5 +1,38 @@
 package ejercicio3;
 
-public class MatriculaService {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class MatriculaService {
+	private List<Usuario> usuariosRegistrados;
+	private Map<String, Campus> campusDisponible;
+
+	public MatriculaService() {
+		this.usuariosRegistrados = new ArrayList<Usuario>();
+		this.campusDisponible = new HashMap<String, Campus>();
+	}
+
+	private void crearCampus() {
+		Campus londres = new Campus("Londres");
+		Campus manchester = new Campus("Manchester");
+		Campus liverpool = new Campus("Liverpool");
+
+		londres.crearProgramas(new String[] { "Informatica", "Medicina", "Marketing", "Artes" }, 1);
+		manchester.crearProgramas(new String[] { "Informatica", "Medicina", "Marketing", "Artes" }, 3);
+		liverpool.crearProgramas(new String[] { "Informatica", "Medicina", "Marketing", "Artes" }, 1);
+
+		campusDisponible.put("Londres", londres);
+		campusDisponible.put("Manchester", manchester);
+		campusDisponible.put("Liverpool", liverpool);
+	}
+
+	public void registrarUsuario(Usuario usuario) {
+		Campus campus = campusDisponible.get(usuario.getCampusElegido());
+		if (campus != null && campus.getPlazasDisponibles(usuario.getProgramaElegido()) > 0) {
+			campus.ocuparPlaza(usuario.getProgramaElegido());
+			usuariosRegistrados.add(usuario);
+		}
+	}
 }
