6c5608ef8ed0cc6d013c5b4fa6ff07930d074bad
KevDeto <contactohuayta@gmail.com>
2025-01-12 01:13:21 -0300

    com

--- Programming_Skills_Level0/src/ejercicio3/MenuService.java
+++ Programming_Skills_Level0/src/ejercicio3/MenuService.java
@@ -1,5 +1,18 @@
 package ejercicio3;
 
 public class MenuService {
+	public void mostrarMenuPrincipal() {
+		System.out.println("Bienvenido al sistema de matrícula universitaria");
+		System.out.println("1. Registrarse\t2. Iniciar sesion\t3. Salir");
+	}
 
+	public void mostrarMenuProgramas() {
+		System.out.println("Seleccione un programa:");
+		System.out.println("1. Informática\t2. Medicina\n3. Marketing\t4. Artes");
+	}
+
+	public void mostrarMenuCampus() {
+		System.out.println("Seleccione un campus:");
+		System.out.println("1. Londres\t2. Manchester\t3. Liverpool");
+	}
 }
