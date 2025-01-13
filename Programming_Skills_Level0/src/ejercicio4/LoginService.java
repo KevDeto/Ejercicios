52e77d5f5255fc6cd01a402afef071df537b5afd
KevDeto <contactohuayta@gmail.com>
2025-01-13 12:25:04 -0300

    terminado la funcionalidad del login

--- Programming_Skills_Level0/src/ejercicio4/LoginService.java
+++ Programming_Skills_Level0/src/ejercicio4/LoginService.java
@@ -1,5 +1,46 @@
 package ejercicio4;
 
-public class LoginService {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
 
+public class LoginService {
+    private Map<String, String> usuarios = new HashMap<>();
+    private int intentos = 0;
+    private int intentosMaximos = 3;
+
+    public void crearUsuario(String username, String password) {
+        usuarios.put(username, password);
+    }
+
+    public boolean login(Scanner scanner) {
+        while (intentos < intentosMaximos) {
+            System.out.print("Ingrese su usuario: ");
+            String username = scanner.nextLine();
+            System.out.print("Ingrese su contraseña: ");
+            String password = scanner.nextLine();
+
+            if (usuarios.containsKey(username) && usuarios.get(username).equals(password)) {
+                System.out.println("Inicio de sesion exitoso.");
+                return true;
+            } else {
+                intentos++;
+                System.out.println("Credenciales incorrectas. Intentos restantes: " + (intentosMaximos - intentos));
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LoginService that = (LoginService) o;
+        return intentos == that.intentos && intentosMaximos == that.intentosMaximos && Objects.equals(usuarios, that.usuarios);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(usuarios, intentos, intentosMaximos);
+    }
 }
