bfbad12cd124f9f083f03bd43e8326e16c1ccce7
KevDeto <contactohuayta@gmail.com>
2025-01-13 12:23:47 -0300

    se termino la funcionalidad

--- Programming_Skills_Level0/src/ejercicio4/Paquete.java
+++ Programming_Skills_Level0/src/ejercicio4/Paquete.java
@@ -1,4 +1,25 @@
 package ejercicio4;
 
 public class Paquete {
+    private final String paqueteID;
+    private final String remitente;
+    private final String destinatario;
+    private final double peso;
+
+    public Paquete( String paqueteID, String sender, String recipient, double weight) {
+        this.paqueteID = paqueteID;
+        this.remitente = sender;
+        this.destinatario = recipient;
+        this.peso = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Paquete{" +
+                ", paqueteID='" + paqueteID + '\'' +
+                "remitente='" + remitente + '\'' +
+                ", destinatario='" + destinatario + '\'' +
+                ", peso=" + peso +
+                '}';
+    }
 }
